a4b05d166be99605eadcfce344557e0fab3d09c8
Marriane791 <annemarrieakeyo42@gmail.com>
2022-09-13 23:56:29 +0300

    [fix]:Correction

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    try:
-        num = int(input("Enter an integer"))
-        print(num)
-    except TypeError as err:
-        print("Nonono", err)
-    
+    raise TypeError
