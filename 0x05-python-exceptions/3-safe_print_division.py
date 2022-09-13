92f2c7464e0297fb251e0c83e12dd24e2791e0bf
Marriane791 <annemarrieakeyo42@gmail.com>
2022-09-13 23:09:00 +0300

    [fix]error addition

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    except ZeroDivisionError:
+    except(TypeError, ZeroDivisionError):
         print("Division by zero")
     finally:
        print("Inside result:{}".format(div))
