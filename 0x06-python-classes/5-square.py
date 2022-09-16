841a773898592f928c0c9bb5b9c3259ffb78e8d6
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-16 22:16:59 +0300

    [fix]:Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    """Using a getter to retrive the size"""
+    @property
+    def size(self):
+        return self.__size  
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+    
+    """Public instance method that print the square in stdout"""
+    def my_print(self):
+        print("{},#".format(self.area()))
+        if self.size == 0:
+            print("")
+
