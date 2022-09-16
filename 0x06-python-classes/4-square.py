3acdc7d8039d51b315e30f4fa821aa5b5c72c972
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-16 21:59:24 +0300

    [fix]:Corrections

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,19 +11,18 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
-    """Retrieving the size"""
+    """Using a getter to retrive the size"""
+    @property
     def size(self):
         return self.__size  
 
     @size.setter
     def size(self, value):
-        if value < 0:
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
         
