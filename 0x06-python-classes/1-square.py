5e37d72290c76271fe12fec88bb4beb83ad1d009
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-16 17:22:02 +0300

    [feat]:Private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defining a square class"""
+class Square:
+    """Defining a constructor which instatiates the object"""
+    def __init__(self, size):
+        """The size of the square is made to be a private variable """
+        self.__size = size
