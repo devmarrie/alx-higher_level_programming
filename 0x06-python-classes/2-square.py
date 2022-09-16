2755cb53bd1e36d45a98cfe3932d1f343a1f1ee9
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-16 21:37:53 +0300

    [fix]:Second trial

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,21 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""Define a class Square."""
 
-"""Defining a class as the blueprint of all the objects we shall define"""
+
 class Square:
-    """Instantiating our class using __init__"""
+    """Represent a square."""
+
     def __init__(self, size=0):
-       """Testing the size input 
-       for errors and 
-       raising them"""
-       if not type(size) == int:
-         raise TypeError("Size must be an integer")
-       elif size < 0:
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
-       self.__size = size
+        self.__size = size
 
    
 
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,23 +1,23 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""Define a class Square."""
 
-"""Defining a class as the blueprint 
-of all the objects we shall define"""
+
 class Square:
-    """Instantiating our class using __init__"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-       """Testing the size input 
-       for errors and raising them"""
+        """Initialize a new square.
 
-       if not type(size) == int:
-         raise TypeError("Size must be an integer")
-       elif size < 0:
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be >= 0")
-       self.__size = size
+        self.__size = size
 
-    """The area method enables us to access 
-    the private variable size and find its area"""
     def area(self):
-        area = self.__size **2
-        print(area)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
