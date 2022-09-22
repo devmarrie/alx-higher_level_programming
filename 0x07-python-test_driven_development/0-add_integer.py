58f3073a24816bcf08c652ef80f8ce7a07d882bc
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-22 23:43:07 +0300

    [feat]test for adding two numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,9 +5,9 @@ def add_integer(a, b=98):
     """
     In this case we are given the value of b but it is not always given.
     """
-    if not isinstance(a, int) or  not isinstance(a, float):
+    if not isinstance(a, int) and  not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or  not isinstance(b, float):
+    if not isinstance(b, int) and  not isinstance(b, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
     
