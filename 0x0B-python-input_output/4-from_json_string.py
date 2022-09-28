bcca6dce1774b86582ac693c2ccba90e77b2036f
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-29 01:16:53 +0300

    [fix}method obj

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -10,4 +10,7 @@ It returns an object (Python data structure) represented by a JSON string
 import json
 
 def from_json_string(my_str):
+    """
+    We convert to an oblect using json.loads()
+    """
     return json.loads(my_str)
