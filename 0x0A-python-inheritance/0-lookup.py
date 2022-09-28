ad7783b8e7c17d864421ba7be85b1b430a6d2831
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-28 16:53:16 +0300

    [feat]:The dir method

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Given a class containing various attributes and methods it returns 
+alist of available attributes and methods of the  object.
+
+"""
+def lookup(obj):
+    """
+    We use the dir() method which returns a list containing strings
+    """
+    print(dir(obj))   
