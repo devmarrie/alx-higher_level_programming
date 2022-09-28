6e57f42d5561d56deba2e089d613f127cb8e0a30
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-29 01:22:31 +0300

    [feat]:obj to textfile

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Writes an object to a textfile
+
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Whenever you write remember to use the with kenyword
+    """
+    with open(filename,"w", encoding="utf-8") as f:
+        return json.dump(my_obj)
+        
