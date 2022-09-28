c25ba4df14c156728c3a896db2cf7aa49ba303e0
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-29 01:10:50 +0300

    [fix]:minor fixes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,6 @@ def write_file(filename="", text=""):
     """
     Using the with keyword for easy closing of the file afterwards
     """
-    with open(filename,encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
         f.closed
