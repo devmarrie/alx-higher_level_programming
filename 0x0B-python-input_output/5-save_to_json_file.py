c857e0cdd62aae3c667d620e17c2d88f6c3d8515
devmarrie <annemarrieakeyo42@gmail.com>
2022-09-29 01:24:07 +0300

    [fix]:adding the file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,5 +13,5 @@ def save_to_json_file(my_obj, filename):
     Whenever you write remember to use the with kenyword
     """
     with open(filename,"w", encoding="utf-8") as f:
-        return json.dump(my_obj)
+        return json.dump(my_obj,f)
         
