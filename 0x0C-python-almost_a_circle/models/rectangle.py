87f83ffdbfac34bed485d6f57e76d19630f8d52d
devmarrie <annemarrieakeyo42@gmail.com>
2022-10-04 06:31:38 +0300

    [feat]:args and kwargs in a square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 """
 import the Base class 
 """
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 """
 Defining a class square that inherits from rectangle
@@ -23,3 +23,22 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    """
+
+    1st argument should be the id attribute
+    2nd argument should be the size attribute
+    3rd argument should be the x attribute
+    4th argument should be the y attribute
+    **kwargs must be skipped if *args exists and is not empty
+    """
+    def update(self, *args, **kwargs):
+        self.id = args[0]
+        self.width = args[1]
+        self.x = args[2]
+        self.y = args[3]
+        if args is  None and len(args) == 0:
+            for key,value in kwargs:
+                print("{}:{}".format(key,value))
+        return ("[Square] ({}) {}/{} - {}".format(self.id,self.width,self.x,self.y))
+        
