a81fa49f5a6f7b23026e24992cac19182986f5ce
devmarrie <annemarrieakeyo42@gmail.com>
2022-10-05 23:27:27 +0300

    [fix]:args and kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,10 +7,14 @@ Defining a class square that inherits from rectangle
 """
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, width=size,height=size)
-    
-    def __str__(self):
-        return ("[Square] ({}) {}/{} - {}".format(self.id,self.x,self.y,self.width))
+        """
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
     """
     Adding a public getter and setter.
     width and the height - with the same value
@@ -24,6 +28,11 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format(self.id,self.x,self.y,self.width))
+    
+    
     """
 
     1st argument should be the id attribute
@@ -33,24 +42,58 @@ class Square(Rectangle):
     **kwargs must be skipped if *args exists and is not empty
     """
     def update(self, *args, **kwargs):
-        self.id = args[0]
-        self.width = args[1]
-        self.x = args[2]
-        self.y = args[3]
-        if args is  None and len(args) == 0:
-            for key,value in kwargs:
-                print("{}:{}".format(key,value))
-        return ("[Square] ({}) {}/{} - {}".format(self.id,self.width,self.x,self.y))
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     """
     Adding a dictonary defination.
     """
     def to_dictionary(self):
-        s_dic = {
+        return {
             "id":self.id,
             "size":self.width,
             "x":self.x,
             "y":self.y
         }
-        return s_dic
-        
+    
+
+if __name__ == "__main__":
+
+    s1 = Square(10, 2, 1)
+    print(s1)
+    s1_dictionary = s1.to_dictionary()
+    print(s1_dictionary)
+    print(type(s1_dictionary))
+
+    s2 = Square(1, 1)
+    print(s2)
+    s2.update(**s1_dictionary)
+    print(s2)
+    print(s1 == s2)
