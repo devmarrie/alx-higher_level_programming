497ae0adf0a6db78172c7bc7f9336c0c418323b1
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 08:26:41 +0300

    [fix]corrwctions

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,4 @@ SELECT `id`, `name`
 FROM `hbtn_0d_usa`.`cities`
 WHERE `name` =
   (SELECT `name` FROM `hbtn_0d_usa`.`states` WHERE `name` = 'California')
-ORDER BY `cities`.`id` ASC;
+ORDER BY `cities`.`id`;
