a7f5e54afb13b260b2f012ad2daec71496f2034b
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 07:35:00 +0300

    [fix]:defining the PK well

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (`id` INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL);
+USE DATABASE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (PRIMARY KEY(`id`), `id` INTEGER  NOT NULL AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL);
