201556f416603e5aa4af33754f79a6e706148d24
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 08:28:35 +0300

    [fix]:Removing the db

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT `id`, `name`
-FROM `hbtn_0d_usa`.`cities`
+FROM `cities`
 WHERE `name` =
-  (SELECT `name` FROM `hbtn_0d_usa`.`states` WHERE `name` = 'California')
-ORDER BY `cities`.`id`;
+  (SELECT `name` FROM `states` WHERE `name` = 'California')
+ORDER BY `id`;
