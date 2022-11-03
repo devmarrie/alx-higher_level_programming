dee5952b2c967d4d5328f8f9b530804efcb8da3f
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 06:37:25 +0300

    [fix]:syntax

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- create user and grants priviledges
 CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost' 
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEDGES 
-ON *.* 
-TO 'user_0d_1'@'localhost';
+   ON *.* 
+   TO 'user_0d_1'@'localhost';
