2305dc31d5a0d5e51118256a2dd0d05589afea74
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 10:21:12 +0300

    [fix]:using inner join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 SELECT g.`name` AS `genre`,
        COUNT(*) AS `number_of_shows`
   FROM `tv_genres` AS g
-       LEFT JOIN `tv_show_genres` AS t
+       INNER JOIN `tv_show_genres` AS t
        ON g.`id` = t.`genre_id`
  GROUP BY g.`name`
  ORDER BY `number_of_shows` DESC;
