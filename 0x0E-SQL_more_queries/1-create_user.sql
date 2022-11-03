5e6c4564879ba3b9a7b66b1aa267f51ef36c4875
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 06:34:53 +0300

    [fix]:alignment trial

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- create user and grants priviledges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER
+IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES 
+ON *.* 
+TO 'user_0d_1'@'localhost';
