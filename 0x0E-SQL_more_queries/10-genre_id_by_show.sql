87dd306780b3b0ee3bc3eddd4c4c4124ac317561
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 09:24:35 +0300

    [fix] based on a dump

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- Import the database dump from hbtn_0d_tvshows
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv.`title`, sh.`genre_id`
+  FROM `tv_shows` AS tv
+        INNER JOIN `tv_show_genres` as sh
+        ON tv.`id` = sh.`show_id`
+ORDER BY tv.`title`, sh.`genre_id`;
+
