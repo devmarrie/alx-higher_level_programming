0acc67c84ed76f98ee91e4b4d882a6b6e5f57a83
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 09:26:40 +0300

    [fix]:as to AS

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv.`title`, sh.`genre_id`
   FROM `tv_shows` AS tv
-        INNER JOIN `tv_show_genres` as sh
+        INNER JOIN `tv_show_genres` AS sh
         ON tv.`id` = sh.`show_id`
 ORDER BY tv.`title`, sh.`genre_id`;
 
