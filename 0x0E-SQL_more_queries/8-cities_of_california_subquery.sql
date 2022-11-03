e8c9e7bafd558d4688946a342c91fb834f233b09
devmarrie <annemarrieakeyo42@gmail.com>
2022-11-03 08:32:34 +0300

    [fix]debug

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 SELECT `id`, `name`
 FROM `cities`
 WHERE `state_id` IN
-  (SELECT `state_id` FROM `states` WHERE `name` = 'California')
+  (SELECT `id` FROM `states` WHERE `name` = 'California')
 ORDER BY `id`;
