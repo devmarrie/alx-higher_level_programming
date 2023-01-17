033cd4c9b5ea6d03d3fc8fe9330e889a0c24312a
devmarrie <annemarrieakeyo42@gmail.com>
2023-01-17 15:06:05 +0300

    from webpage to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(path, body, 'utf-8');
+  }
+});
