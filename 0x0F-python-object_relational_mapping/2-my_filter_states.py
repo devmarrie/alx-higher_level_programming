f958623c5d9da508e28eda87d3ab5b21f68de40b
devmarrie <annemarrieakeyo42@gmail.com>
2023-01-30 02:14:31 +0300

    col not quoted

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in the states table of hbtn_0e_0_usa 
-where name matches the argument.
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1],port=3306, host="localhost",
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` WHERE name LIKE {:s} ORDER BY \
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
     id ASC".format(sys.argv[4]))
     states = c.fetchall()
     for state in states:
@@ -18,4 +22,3 @@ if __name__ == "__main__":
             print(state)
     c.close()
     db.close()
-
