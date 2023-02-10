4484426ecb764d7d2967c3e0c09421a486446125
devmarrie <annemarrieakeyo42@gmail.com>
2023-02-10 15:47:01 +0300

    wow

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # curl sends GET req to URL, displays response body
+# display a custom header
 curl -sH "X-School-User-Id: 98" "$1"
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,7 +10,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    email = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=value)
+    r = requests.post(url, data=email)
     print(r.text)
